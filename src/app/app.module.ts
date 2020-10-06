cd61ca8a9648185a7a1fb23633cb99946b127a6b
Arece7 <arece73@gmail.com>
2020-10-06 12:50:22 +0530

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishDetailsComponent } from './dish-details/dish-details.component';
+import { DishService } from './services/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +23,7 @@ import { DishDetailsComponent } from './dish-details/dish-details.component';
     MatToolbarModule,FlexLayoutModule,MatListModule,
     MatGridListModule,MatCardModule,MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
