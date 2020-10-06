45d982e13ba4c6f00bdfbdead730b8fff8a66da3
Arece7 <arece73@gmail.com>
2020-10-06 20:22:29 +0530

    router basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,38 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import 'hammerjs';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { DishDetailsComponent } from './dish-details/dish-details.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishDetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,BrowserAnimationsModule,
     MatToolbarModule,FlexLayoutModule,MatListModule,
-    MatGridListModule,MatCardModule,MatButtonModule
+    MatGridListModule,MatCardModule,MatButtonModule,AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
