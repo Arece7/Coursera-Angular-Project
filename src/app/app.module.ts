29913c1baf584ed990a407b181863a6af9cf991e
Arece7 <arece73@gmail.com>
2020-10-07 14:42:19 +0530

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -38,8 +43,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,BrowserAnimationsModule,
-    MatToolbarModule,FlexLayoutModule,MatListModule,
-    MatGridListModule,MatCardModule,MatButtonModule,AppRoutingModule,MatDialogModule
+    MatToolbarModule,FlexLayoutModule,MatListModule, MatFormFieldModule,
+    MatGridListModule,MatCardModule,MatButtonModule,AppRoutingModule,MatDialogModule,MatInputModule,
+    FormsModule,MatCheckboxModule,
   ],
   entryComponents: [
     LoginComponent
