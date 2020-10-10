abf152d72bf9bd2baf30cc8fcf49de9b2e1645e9
Arece7 <arece73@gmail.com>
2020-10-10 22:11:19 +0530

    custom directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service'
 
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +51,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,BrowserAnimationsModule,
